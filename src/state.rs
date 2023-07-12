58521099fa614bf291f11c83f65be9ff8c66a91c
Ned Wolpert <ned.wolpert@gmail.com>
2023-07-12 07:51:47 -0700

    Moved state to it's own location

---
+++ src/state.rs
@@ -0,0 +1,18 @@
+use bevy::prelude::{Component, Resource, States};
+
+// Enum that will be used as a global state for the game
+#[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
+pub enum GameState {
+    #[default]
+    Splash,
+    Menu,
+    Game,
+}
+
+// One of the two settings that can be set through the menu. It will be a resource in the app
+#[derive(Resource, Debug, Component, PartialEq, Eq, Clone, Copy)]
+pub enum DisplayQuality {
+    Low,
+    Medium,
+    High,
+}
